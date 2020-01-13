08b82c02917cd52b151be034bd88338d16031988
jeremythuff <jeremythuff@gmail.com>
2020-01-13 15:55:08 -0600

    Updated build process

--- bld/build-elements.js
+++ bld/build-elements.js
@@ -5,10 +5,14 @@ const concat = require('concat');
   const files = [
     'dist/weaver-components/runtime-es2015.js',
     'dist/weaver-components/polyfills-es2015.js',
+    // 'dist/weaver-components/styles-es2015.js',
     'dist/weaver-components/scripts.js',
+    // 'dist/weaver-components/vendor-es2015.js',
     'dist/weaver-components/main-es2015.js'
   ];
 
   await concat(files, 'dist/weaver-components.js');
+
+  await fs.remove("dist/weaver-components");
 
 })();
