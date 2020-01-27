cc1b06ff7bfd921615e2096037ad6a761cd76b41
Jeremy Huff <jeremythuff@gmail.com>
2020-01-27 00:36:30 -0600

    WIP Increasing themable props

--- lib/wvr-elements/src/lib/wvr-nav-list/wvr-nav-list.component.ts
+++ lib/wvr-elements/src/lib/wvr-nav-list/wvr-nav-list.component.ts
@@ -11,17 +11,37 @@ import { Alignment } from './alignment.enum';
 })
 export class WvrNavListComponent extends WvrBaseComponent {
 
+  private readonly COLOR_CSS_VAR: string = '--color';
+
   @Input() aligned: Alignment = Alignment.LEFT;
 
   Alignment = Alignment;
 
   @Input() vertical = false;
 
+  @Input()
+  set color(v: string) {
+    this.themeSettings.set(this.COLOR_CSS_VAR, v);
+  }
+
+  get color(): string {
+    return this.themeSettings.get(this.COLOR_CSS_VAR);
+  }
+
+  private themeSettings: Map<string, string> = new Map<string, string>();
+
   constructor(injector: Injector) {
     super(injector);
   }
 
   ngOnInit() {
+    this.applyTheme();
+  }
+
+  private applyTheme(): void {
+    this.themeSettings.forEach((varVal, varName) => {
+      this.elem.nativeElement.style.setProperty(varName, varVal);
+    });
   }
 
 }
