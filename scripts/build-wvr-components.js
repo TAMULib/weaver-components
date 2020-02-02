b697871193aabf53f3aba56b35e03b1521b41c3b
William Welling <wwelling@library.tamu.edu>
2020-02-02 10:52:12 -0600

    minor update to wvr components build script

--- scripts/build-wvr-components.js
+++ scripts/build-wvr-components.js
@@ -1,16 +1,18 @@
-const fs = require('fs-extra');
+// const fs = require('fs-extra');
 const concat = require('concat');
 
 (async function build() {
   const files = [
     'dist/weaver-components/runtime-es5.js',
     'dist/weaver-components/polyfills-es5.js',
-    // 'dist/weaver-components/styles-es2015.js',
+    // 'dist/weaver-components/styles-es5.js',
     'dist/weaver-components/scripts.js',
-    // 'dist/weaver-components/vendor-es2015.js',
+    // 'dist/weaver-components/vendor-es5.js',
     'dist/weaver-components/main-es5.js'
   ];
 
   await concat(files, 'dist/weaver-components.js');
+
+  // await fs.remove("dist/weaver-components");
 
 })();
