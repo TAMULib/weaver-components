8f6b5548c5bba31c95bda0ad670f80d7079127dc
jeremythuff <jeremythuff@gmail.com>
2020-02-03 16:03:10 -0600

    Added new wvr-text component

---
+++ projects/wvr-elements/src/lib/wvr-text/wvr-text.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WvrTextComponent } from './wvr-text.component';
+
+describe('WvrTextComponent', () => {
+  let component: WvrTextComponent;
+  let fixture: ComponentFixture<WvrTextComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WvrTextComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WvrTextComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
