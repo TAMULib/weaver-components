e6de33c276459712689def86d16f42dc480b77dd
jeremythuff <jeremythuff@gmail.com>
2020-02-11 16:22:32 -0600

    Added links to stories

--- projects/wvr-elements/src/lib/wvr-header/wvr-header.component.stories.ts
+++ projects/wvr-elements/src/lib/wvr-header/wvr-header.component.stories.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line:no-default-import
 import wrapper from '../shared/story-wrapper';
-import { text, withKnobs } from '@storybook/addon-knobs';
+import { array, color, object, text, withKnobs } from '@storybook/addon-knobs';
 
 // tslint:disable-next-line:no-default-export
 export default {
@@ -14,11 +14,33 @@ export const defaultHeader = wrapper(() => `
 `);
 
 export const customizedHeader = wrapper(() => {
-  const title = text('Header Title', 'John Doe');
-  const primary = text('Primary Color', 'blue');
-  const secondary = text('Secondary Color', 'gray');
+  const logoSrc = text('Logo Source', 'weaver-components/assets/weaver-w.svg', 'General');
+  const logoText = text('Logo Text', 'Logo Text', 'General');
+  const logoHref = text('Logo HREF', '#relative-url', 'General');
+  const title = text('Header Title', 'Page Title', 'General');
+
+  const topLinks = array('Top Links', [], '|', 'General');
+  const bottomLinks = array('Bottom Links', [], '|', 'General');
+
+  const primary = color('Primary Color', 'var()', 'Theme');
+  const secondary = color('Secondary Color', 'var()', 'Theme');
+  const gray = color('Gray Color', 'var()', 'Theme');
   const template = `
-<wvr-header header-title="${title}" primary="${primary}" secondary="${secondary}">
+<wvr-header logo-src="${logoSrc}" logo-text="${logoText}" logo-href="${logoHref}" header-title="${title}" primary="${primary}" secondary="${secondary}" gray="${gray}">
+  <wvr-nav-list top-navigation aligned="RIGHT">
+  ${topLinks.map(l =>
+    `<wvr-nav-li href="#${l}">
+      <wvr-text value="${l}"></wvr-text>
+    </wvr-nav-li>`
+  )}
+  </wvr-nav-list>
+  <wvr-nav-list bottom-navigation>
+  ${bottomLinks.map(l =>
+    `<wvr-nav-li href="#${l}">
+      <wvr-text value="${l}"></wvr-text>
+    </wvr-nav-li>`
+  )}
+  </wvr-nav-list>
 </wvr-header>
   `;
 
@@ -27,5 +49,27 @@ export const customizedHeader = wrapper(() => {
 
 export const headerWithLinks = wrapper(() => `
 <wvr-header>
+  <wvr-nav-list top-navigation aligned="RIGHT">
+    <wvr-nav-li href="#about-us">
+      <wvr-text value="About"></wvr-text>
+    </wvr-nav-li>
+    <wvr-nav-li href="#hours">
+      <wvr-text value="Hours"></wvr-text>
+    </wvr-nav-li>
+    <wvr-nav-li href="#contact">
+      <wvr-text value="Contact"></wvr-text>
+    </wvr-nav-li>
+  </wvr-nav-list>
+  <wvr-nav-list bottom-navigation>
+    <wvr-nav-li href="#1">
+      <wvr-text value="Link 1"></wvr-text>
+    </wvr-nav-li>
+    <wvr-nav-li href="#2">
+      <wvr-text value="Link 2"></wvr-text>
+    </wvr-nav-li>
+    <wvr-nav-li href="#3">
+      <wvr-text value="Link 3"></wvr-text>
+    </wvr-nav-li>
+  </wvr-nav-list>
 </wvr-header>
 `);
