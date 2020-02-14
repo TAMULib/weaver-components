f6943c32dba941eaf3a4c620dfc0676e2b1a71c9
William Welling <wwelling@library.tamu.edu>
2020-02-14 12:37:25 -0600

    use input annotations, minor clean, upgrade dependencies

--- projects/wvr-elements/src/lib/wvr-it-works/wvr-it-works.component.ts
+++ projects/wvr-elements/src/lib/wvr-it-works/wvr-it-works.component.ts
@@ -1,21 +1,16 @@
-import { Component, Input } from '@angular/core';
-import { wvrThemeHostBindings, wvrThemeInputs } from '../shared/wvr-styles';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'wvr-it-works-element',
   templateUrl: './wvr-it-works.component.html',
-  styleUrls: ['./wvr-it-works.component.scss'],
-  inputs: [
-    ...wvrThemeInputs
-  ],
-  host: {
-    ...wvrThemeHostBindings
-  }
+  styleUrls: ['./wvr-it-works.component.scss']
 })
 export class WvrItWorksComponent {
 
   title = 'it-works-component';
 
   @Input() text = 'Weaver Components Work';
+
+  @HostBinding('style.--wvr-primary') @Input() primary;
 
 }
--- projects/wvr-elements/src/lib/wvr-nav-list/wvr-nav-li/wvr-nav-li.component.ts
+++ projects/wvr-elements/src/lib/wvr-nav-list/wvr-nav-li/wvr-nav-li.component.ts
@@ -1,19 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { wvrThemeHostBindings, wvrThemeInputs } from '../../shared/wvr-styles';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'wvr-nav-li-element',
   templateUrl: './wvr-nav-li.component.html',
-  styleUrls: ['./wvr-nav-li.component.scss'],
-  inputs: [
-    ...wvrThemeInputs
-  ],
-  host: {
-    ...wvrThemeHostBindings
-  }
+  styleUrls: ['./wvr-nav-li.component.scss']
 })
 export class WvrNavLiComponent {
 
   @Input() href: string;
+
+  @HostBinding('style.--wvr-blue') @Input() blue;
+
+  @HostBinding('style.--wvr-primary') @Input() primary;
+
+  @HostBinding('style.--wvr-dark') @Input() dark;
 
 }
