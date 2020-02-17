e84105101c52f5b04874ea93e51070e2973bfa10
Rincy Mathew <rmathew@library.tamu.edu>
2020-02-17 16:15:34 -0600

    WIP # Please enter the commit message for your changes. Lines starting

--- projects/wvr-elements/src/lib/wvr-footer/wvr-footer.component.ts
+++ projects/wvr-elements/src/lib/wvr-footer/wvr-footer.component.ts
@@ -15,7 +15,28 @@ export class WvrFooterComponent implements OnInit {
   }
 
   positionSelf(): void {
-    console.log(this.elementRef);
+    const parentElement: HTMLElement = this.elementRef.nativeElement.offsetParent;
+    const wvrFooter: HTMLElement = parentElement.querySelector('footer.wvr-footer');
+    const selfHeight = wvrFooter.offsetHeight;
+    const viewPortSize = window.innerHeight; //document.body.offsetHeight;
+
+    if (parentElement.offsetHeight <= viewPortSize) {
+      console.log('CONTENT IS SMALLER');
+      // position = vieportSize - self Height
+      const footerPosition = viewPortSize - selfHeight;
+      //wvrFooter.style.position = 'absolute';
+      //wvrFooter.style.top = `${footerPosition}px`;
+
+    } else {
+      console.log('CONTENT IS LARGER');
+    }
+
+    console.log(wvrFooter);
+    console.log("parentElement", parentElement);
+    console.log('parentElement.offsetHeight', parentElement.offsetHeight);
+    console.log("selfHeight", selfHeight);
+    console.log('this.elementRef', this.elementRef);
+    console.log('window.innerHeight', window.innerHeight);
   }
 
   @HostListener('window:resize', ['$event']) onResize(event): void {
