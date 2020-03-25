50f42ab503be4caa806b5ed3f089686a1c6e4708
Rincy Mathew <rmathew@library.tamu.edu>
2020-03-25 13:06:17 -0500

    Removed unused import

--- projects/wvr-elements/src/lib/wvr-text/wvr-text.component.ts
+++ projects/wvr-elements/src/lib/wvr-text/wvr-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 /**
  * The Weaver Text Component allows for a node based textual entry. This will support i18n in the future.
