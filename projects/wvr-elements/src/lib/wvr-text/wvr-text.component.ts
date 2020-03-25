30e1a831a33a894a6be09cb9d11f219248908d0e
Rincy Mathew <rmathew@library.tamu.edu>
2020-03-25 11:34:35 -0500

    Added styling for wvr-text node

--- projects/wvr-elements/src/lib/wvr-text/wvr-text.component.ts
+++ projects/wvr-elements/src/lib/wvr-text/wvr-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
 
 /**
  * The Weaver Text Component allows for a node based textual entry. This will support i18n in the future.
@@ -12,5 +12,54 @@ export class WvrTextComponent {
 
   /** The text value to be displayed in the text node. */
   @Input() value: string;
+
+  /** Allows for the override of font-size property for wvr-text */
+  @HostBinding('style.--wvr-text-font-size') @Input() fontSize;
+
+  /** Allows for the override of font-family property for wvr-text */
+  @HostBinding('style.--wvr-text-font-family') @Input() fontFamily;
+
+  /** Allows for the override of font-style property for wvr-text */
+  @HostBinding('style.--wvr-text-font-style') @Input() fontStyle;
+
+  /** Allows for the override of font-variant property for wvr-text */
+  @HostBinding('style.--wvr-text-font-variant') @Input() fontVariant;
+
+  /** Allows for the override of font-weight property for wvr-text */
+  @HostBinding('style.--wvr-text-font-weight') @Input() fontWeight;
+
+  /** Allows for the override of font-stretch property for wvr-text */
+  @HostBinding('style.--wvr-text-font-stretch') @Input() fontStretch;
+
+  /** Allows for the override of line-height property for wvr-text */
+  @HostBinding('style.--wvr-text-line-height') @Input() lineHeight;
+
+  // text rules
+  /** Allows for the override of color property for wvr-text */
+  @HostBinding('style.--wvr-text-color') @Input() textColor;
+
+  /** Allows for the override of text-align property for wvr-text */
+  @HostBinding('style.--wvr-text-text-align') @Input() textAlign;
+
+  /** Allows for the override of text-decoration property for wvr-text */
+  @HostBinding('style.--wvr-text-text-decoration') @Input() textDecoration;
+
+  /** Allows for the override of text-transform property for wvr-text */
+  @HostBinding('style.--wvr-text-text-transform') @Input() textTransform;
+
+  /** Allows for the override of text-indent property for wvr-text */
+  @HostBinding('style.--wvr-text-text-indent') @Input() textIndent;
+
+  /** Allows for the override of letter-spacing property for wvr-text */
+  @HostBinding('style.--wvr-text-letter-spacing') @Input() letterSpacing;
+
+  /** Allows for the override of direction property for wvr-text */
+  @HostBinding('style.--wvr-text-direction') @Input() textDirection;
+
+  /** Allows for the override of text-shadow property for wvr-text */
+  @HostBinding('style. --wvr-text-text-shadow') @Input() textShadow;
+
+  /** Allows for the override of word-spacing property for wvr-text */
+  @HostBinding('style.--wvr-text-word-spacing') @Input() wordSpacing;
 
 }
