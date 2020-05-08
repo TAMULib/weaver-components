5f104299fab08e4438b5af1a8c39a0269ead096f
jeremythuff <jeremythuff@gmail.com>
2020-05-08 14:40:59 -0500

    WIP

--- projects/wvr-elements/src/lib/shared/wvr-abstract-base.component.ts
+++ projects/wvr-elements/src/lib/shared/wvr-abstract-base.component.ts
@@ -1,16 +1,28 @@
-import { HostBinding, Input } from '@angular/core';
+import { OnChanges, SimpleChanges } from '@angular/core';
 
-export abstract class WvrAbstractBaseComponent {
+export abstract class WvrAbstractBaseComponent {  // implements OnChanges
 
-    /** Allows for the override of the --tl-default-font-size css variable. */
-    @HostBinding('style.--tl-font-size') _fontSize; // (this.inheritFontStyle === 'false') ? '23px' : 'inherit';
+    // /** Allows for the override of the --tl-default-font-size css variable. */
+    // @HostBinding('style.--tl-font-size') _fontSize; // (this.inheritFontStyle === 'false') ? '23px' : 'inherit';
 
-    /** Allows for the override of the --tl-font-family-sans-serif css variable. */
-    @HostBinding('style.--tl-font-family-sans-serif') _fontFamily; // = (this.inheritFontStyle === 'false') ? 'sans-serif' : 'inherit';
+    // /** Allows for the override of the --tl-font-family-sans-serif css variable. */
+    // @HostBinding('style.--tl-font-family-sans-serif') _fontFamily; // = (this.inheritFontStyle === 'false') ? 'sans-serif' : 'inherit';
 
-    @Input() set inheritFontStyle(value: 'true' | 'false') {
-      this._fontSize = (value === 'true') ? 'inherit' : 'var(--tl-default-font-size)';
-      this._fontFamily = (value === 'true') ? 'inherit' : 'var(--tl-default-font-family-sans-serif)';
-    }
+    // @Input() set inheritFontStyle(value: 'true' | 'false') {
+    //   this._fontSize = (value === 'true') ? 'inherit' : 'var(--tl-default-font-size)';
+    //   this._fontFamily = (value === 'true') ? 'inherit' : 'var(--tl-default-font-family-sans-serif)';
+    // }
+
+    // ngOnChanges(changes: SimpleChanges): void {
+    //   Object.keys(changes)
+    //   .forEach(c => {
+    //     Object.getOwnPropertyNames(this)
+    //     .forEach(p => {
+    //       if(c === p) {
+    //         console.log(typeof p);
+    //       }
+    //     });
+    //   });
+    // }
 
 }
--- projects/wvr-elements/src/lib/wvr-dropdown/wvr-dropdown.component.ts
+++ projects/wvr-elements/src/lib/wvr-dropdown/wvr-dropdown.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, Input, ViewChild } from '@angular/core';
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
+import { WvrAbstractBaseComponent } from '../shared/wvr-abstract-base.component';
 // import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -6,14 +8,36 @@ import { ChangeDetectorRef, Component } from '@angular/core';
   templateUrl: './wvr-dropdown.component.html',
   styleUrls: ['./wvr-dropdown.component.scss']
 })
-export class WvrDropdownComponent {
+export class WvrDropdownComponent extends WvrAbstractBaseComponent {
 
-  detectChanges(): void {
-    this.ref.detectChanges();
+  @ViewChild(NgbDropdown) private dropdown: NgbDropdown;
+
+  @Input() btnType: 'primary' | 'seconday' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'link' | 'plain' = 'plain';
+
+  @Input() toggleOn: 'click' | 'mouseover' = 'click';
+
+  @HostListener('mouseenter', ['$event']) @HostListener('mouseleave', ['$event']) hoverOpen($event): void {
+    if (this.toggleOn === 'mouseover') {
+      this.dropdown.toggle();
+    } else {
+      $event.stopPropagation();
+    }
   }
 
-  constructor(private ref: ChangeDetectorRef) {
+  @HostListener('click', ['$event']) clickOpen($event: Event): void {
+    if (this.toggleOn === 'mouseover') {
+      $event.stopPropagation();
+    } else {
+      this.dropdown.toggle();
+    }
+  }
 
+  constructor(private cdRef: ChangeDetectorRef) {
+    super();
+  }
+
+  detectChanges(): void {
+    this.cdRef.detectChanges();
   }
 
 }
