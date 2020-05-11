464a7cc8986d86b12bf8396520a3812c418deb70
jeremythuff <jeremythuff@gmail.com>
2020-05-11 02:48:27 -0500

    Removed commented code

--- projects/wvr-elements/src/lib/shared/wvr-abstract-base.component.ts
+++ projects/wvr-elements/src/lib/shared/wvr-abstract-base.component.ts
@@ -1,28 +1,5 @@
 import { OnChanges, SimpleChanges } from '@angular/core';
 
-export abstract class WvrAbstractBaseComponent {  // implements OnChanges
-
-    // /** Allows for the override of the --tl-default-font-size css variable. */
-    // @HostBinding('style.--tl-font-size') _fontSize; // (this.inheritFontStyle === 'false') ? '23px' : 'inherit';
-
-    // /** Allows for the override of the --tl-font-family-sans-serif css variable. */
-    // @HostBinding('style.--tl-font-family-sans-serif') _fontFamily; // = (this.inheritFontStyle === 'false') ? 'sans-serif' : 'inherit';
-
-    // @Input() set inheritFontStyle(value: 'true' | 'false') {
-    //   this._fontSize = (value === 'true') ? 'inherit' : 'var(--tl-default-font-size)';
-    //   this._fontFamily = (value === 'true') ? 'inherit' : 'var(--tl-default-font-family-sans-serif)';
-    // }
-
-    // ngOnChanges(changes: SimpleChanges): void {
-    //   Object.keys(changes)
-    //   .forEach(c => {
-    //     Object.getOwnPropertyNames(this)
-    //     .forEach(p => {
-    //       if(c === p) {
-    //         console.log(typeof p);
-    //       }
-    //     });
-    //   });
-    // }
+export abstract class WvrAbstractBaseComponent {
 
 }
