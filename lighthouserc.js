6e6296ee85329435c8b2dc19abdc0b9dae5ae5c2
Rincy Mathew <rmathew@library.tamu.edu>
2020-05-14 08:46:49 -0500

    WIP lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './',
+      // url: ['http://localhost:3000/'],
+      // startServerCommand: 'rails server -e production',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+      // token:
+    },
+  },
+};
+
+
