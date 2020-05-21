cbb19f7d8d540d4da5f7b822918bc833f2458d9e
Rincy Mathew <rmathew@library.tamu.edu>
2020-05-21 16:46:55 -0500

    WIP

--- scripts/build-wvr-components-docs.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-const docPath = 'static/docs';
-
-(async function build() {
-  fs.ensureDir(`${docPath}/`);
-  fs.copy("index-doc.html", `${docPath}/index.html`);
-  fs.copy("src/assets", `${docPath}/assets`);
-})();
