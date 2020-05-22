51ffb12bec7f79dbee0b011896349292f026bfc6
Rincy Mathew <rmathew@library.tamu.edu>
2020-05-22 10:46:43 -0500

    Work In Progress

--- scripts/build-wvr-components-docs.js
+++ scripts/build-wvr-components-docs.js
@@ -3,6 +3,6 @@ const docPath = 'static/docs';
 
 (async function build() {
   fs.ensureDir(`${docPath}/`);
-  fs.copy("index-doc.html", `${docPath}/index.html`);
+  fs.copy("index-docs.html", `${docPath}/index.html`);
   fs.copy("src/assets", `${docPath}/assets`);
 })();
