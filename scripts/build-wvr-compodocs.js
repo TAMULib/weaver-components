c1cacc5de6d3fca2b355ac370efb3d4016f78afd
Rincy Mathew <rmathew@library.tamu.edu>
2020-05-26 12:09:48 -0500

    WIP to remove development folder after content is copied

--- scripts/build-wvr-compodocs.js
+++ scripts/build-wvr-compodocs.js
@@ -5,7 +5,4 @@ const compodocPath = './development';
 (async function build() {
   fs.ensureDir(`${docsPath}/`) && fs.ensureDir(`${compodocPath}/`);
   fs.copy(`${compodocPath}/`, `${docsPath}/development`);
-  fs.remove(`${compodocPath}/`, err => {
-    if (err) return console.error(err);
-  });
 })();
