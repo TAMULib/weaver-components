d608b00c7f93288ed2918f4202d119bac659b6fc
jeremythuff <jeremythuff@gmail.com>
2020-05-27 17:23:29 -0500

    Paths working locally on static assets

--- scripts/build-wvr-components-static.js
+++ scripts/build-wvr-components-static.js
@@ -1,11 +1,13 @@
 const fs = require('fs-extra');
-const docPath = 'static/weaver-components/docs';
-const reportPath = 'static/weaver-components/reports';
+const staticPath = 'static/weaver-components';
+const docPath = `${staticPath}/docs`;
+const reportPath = `${staticPath}/reports`;
 
 (async function build() {
   fs.ensureDir(`${docPath}/`);
   fs.ensureDir(`${reportPath}`);
 
+  fs.copy("index-static.html", `${staticPath}/index.html`);
   fs.copy("index-docs.html", `${docPath}/index.html`);
   fs.copy("index-reports.html", `${reportPath}/index.html`);
   fs.copy("src/assets", `${docPath}/assets`);
