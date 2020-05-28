7e94163dc462b190aacec9bfb2e4cbd58c5f4d12
jeremythuff <jeremythuff@gmail.com>
2020-05-28 12:13:44 -0500

    Further changed paths in reports and docs

--- scripts/build-wvr-compodocs.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-const docsPath = 'static/docs';
-const compodocPath = './development';
-
-(async function build() {
-  fs.ensureDir(`${docsPath}/`) && fs.ensureDir(`${compodocPath}/`);
-  fs.copy(`${compodocPath}/`, `${docsPath}/development`);
-})();
