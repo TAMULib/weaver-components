c30a1d8e26a817241421b508edf9b9755cd56256
jeremythuff <jeremythuff@gmail.com>
2020-07-14 10:37:40 -0500

    eof nl, removed unsued param

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   entryComponents: []
 })
 export class AppModule {
-  ngDoBootstrap(app): void {
+  ngDoBootstrap(): void {
     // OVERRIDE
   }
 }
