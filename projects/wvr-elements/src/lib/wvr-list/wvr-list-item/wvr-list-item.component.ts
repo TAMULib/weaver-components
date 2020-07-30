f38906502ea9e9fc7e9786d3a93b90d6807debdc
Rincy Mathew <rmathew@library.tamu.edu>
2020-07-30 14:05:53 -0500

    Removed the active event handler and renderer class

--- projects/wvr-elements/src/lib/wvr-list/wvr-list-item/wvr-list-item.component.ts
+++ projects/wvr-elements/src/lib/wvr-list/wvr-list-item/wvr-list-item.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'wvr-list-item-element',
@@ -21,7 +21,7 @@ export class WvrListItemComponent implements AfterViewInit {
 
   @Input() customContentSmallText: string;
 
-  constructor(private readonly ref: ElementRef, private readonly cdRef: ChangeDetectorRef , private readonly renderer: Renderer2) {
+  constructor(private readonly ref: ElementRef, private readonly cdRef: ChangeDetectorRef) {
     this.parent = (ref.nativeElement as HTMLElement).closest('wvr-list');
 
     const listTypeAttribute = this.parent ? this.parent.getAttribute('list-type') : undefined ;
@@ -35,19 +35,6 @@ export class WvrListItemComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.cdRef.detectChanges();
-  }
-
-  /**
-   * A handler method for the `click` event for a list group item.
-   */
-  @HostListener('document:click', ['$event']) onClick($event): void {
-    $event.preventDefault();
-    const listGroupItemElement = (this.ref.nativeElement as HTMLElement).querySelector('.list-group-item');
-    if (this.ref.nativeElement.contains($event.target)) {
-      this.renderer.addClass(listGroupItemElement, 'active');
-    } else {
-      this.renderer.removeClass(listGroupItemElement, 'active');
-    }
   }
 
 }
