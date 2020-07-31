a78891718b5d827abe9322024729d0fbb317821e
William Welling <wwelling@library.tamu.edu>
2020-07-31 18:20:27 -0500

    need config at root as well

--- scripts/build-wvr-components-static.js
+++ scripts/build-wvr-components-static.js
@@ -9,6 +9,7 @@ const reportPath = `${weaverComponentsPath}/reports`;
   fs.ensureDir(`${reportPath}`);
 
   fs.copy("src/config-docs.json", `${weaverComponentsPath}/config.json`);
+  fs.copy("src/config-docs.json", `${staticPath}/config.json`);
   fs.copy("src/index-static.html", `${staticPath}/index.html`);
   fs.copy("src/index-docs.html", `${docPath}/index.html`);
   fs.copy("src/index-reports.html", `${reportPath}/index.html`);
