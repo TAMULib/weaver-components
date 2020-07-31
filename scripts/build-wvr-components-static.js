7865faeea31e2b6cad3a65e9441f818ce9ccc8ad
William Welling <wwelling@library.tamu.edu>
2020-07-31 18:13:26 -0500

    config.json under weaver components

--- scripts/build-wvr-components-static.js
+++ scripts/build-wvr-components-static.js
@@ -8,7 +8,7 @@ const reportPath = `${weaverComponentsPath}/reports`;
   fs.ensureDir(`${docPath}/`);
   fs.ensureDir(`${reportPath}`);
 
-  fs.copy("src/config-docs.json", `${staticPath}/config.json`);
+  fs.copy("src/config-docs.json", `${weaverComponentsPath}/config.json`);
   fs.copy("src/index-static.html", `${staticPath}/index.html`);
   fs.copy("src/index-docs.html", `${docPath}/index.html`);
   fs.copy("src/index-reports.html", `${reportPath}/index.html`);
