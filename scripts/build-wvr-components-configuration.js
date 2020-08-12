0cdd5a0c85b7f2b20b9c8f9cc3fa35197f922dd7
jeremythuff <jeremythuff@gmail.com>
2020-08-12 01:16:03 -0500

    Building configs for dev

--- scripts/build-wvr-components-configuration.js
+++ scripts/build-wvr-components-configuration.js
@@ -2,16 +2,24 @@ const dotEnv = require('dotenv-override-true');
 const { exec } = require('child_process');
 const fs = require('fs-extra');
 
-dotEnv.config({
+let defaults = dotEnv.config({
   path: `${process.cwd()}/defaults.env`
 });
 
 if(process.argv[2]) {
-  dotEnv.config({
+  defaults = dotEnv.config({
     path: `${process.cwd()}/${process.argv[2]}`
   });
 }
 
-exec(`${process.cwd()}/docker-entrypoint`, () => {
-  fs.copy('dist/bundle/latest/config.json', `src/config.json`);
-});
+let configTemplate = fs.readFileSync('./src/config-template.json', 'utf8');
+
+Object.keys(defaults.parsed)
+  .forEach(key => {
+    configTemplate = configTemplate.replace(`$${key}`, defaults.parsed[key])
+  });
+
+fs.writeFile('./dist/bundle/1x/config.json', configTemplate);
+fs.writeFile('./dist/bundle/latest/config.json', configTemplate);
+fs.writeFile('./static/weaver-components/docs/usage/config.json', configTemplate);
+fs.writeFile('./src/config.json', configTemplate);
