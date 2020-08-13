def04eca776baf4ba1c011488a026a9fbdef2455
William Welling <wwelling@library.tamu.edu>
2020-08-13 13:12:03 -0500

    remove redundant imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
 import { WvrLibModule } from '../../projects/wvr-elements/src/public-api';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    WvrLibModule,
-    NgbModule
+    WvrLibModule
   ],
   exports: [],
   providers: [],
   declarations: [],
   bootstrap: [],
   entryComponents: [],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: []
 })
 export class AppModule {
   ngDoBootstrap(): void {
