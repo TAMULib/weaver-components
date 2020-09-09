7182e82978a2499e1fe9b3d57145b00b23282848
jeremythuff <jeremythuff@gmail.com>
2020-09-09 16:39:37 -0500

    WIP provide mobile menu

--- projects/wvr-elements/src/lib/shared/utility/decorators.utilty.ts
+++ projects/wvr-elements/src/lib/shared/utility/decorators.utilty.ts
@@ -1,15 +1,19 @@
-const debounce = (delay = 300): MethodDecorator => (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
-  const timeoutKey = Symbol();
-  const original = descriptor.value;
-  descriptor.value = (...args) => {
-    // tslint:disable-next-line:no-invalid-this
-    clearTimeout(this[timeoutKey]);
-    // tslint:disable-next-line:no-invalid-this
-    this[timeoutKey] = setTimeout(() => original.apply(this, args), delay);
-  };
+// tslint:disable-next-line:only-arrow-functions
+function debounce(delay = 300): MethodDecorator {
+    return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const timeoutKey = Symbol();
+    const original = descriptor.value;
+    // tslint:disable-next-line:typedef
+    descriptor.value = function(...args) {
+      // tslint:disable-next-line:no-invalid-this
+      clearTimeout(this[timeoutKey]);
+      // tslint:disable-next-line:no-invalid-this
+      this[timeoutKey] = setTimeout(() => original.apply(this, args), delay);
+    };
 
-  return descriptor;
-};
+    return descriptor;
+  };
+}
 
 export {
   debounce
