0cd9031203cf07905dc3d2f07a53ca9991917518
jeremythuff <jeremythuff@gmail.com>
2020-10-16 13:08:32 -0500

    Fixed enviroment driven configuration

--- scripts/start-docker.js
+++ scripts/start-docker.js
@@ -4,7 +4,7 @@ const package = require('../package.json');
 const tagBase = process.env.npm_package_config_DOCKER_SERVER.length ? 
                 `${process.env.npm_package_config_DOCKER_SERVER}:` : 
                 '';
-const dockerCmd = `docker run -p 8080:80 -t ${tagBase}${package.version}`;
+const dockerCmd = `docker run --env-file=defaults-dev-overrides.env -p 8080:80 -t ${tagBase}${package.version}`;
 
 shell.config.fatal = true;
 
