74c7192dd8afe332782ea59f107c88808d2d486b
Rincy Mathew <rmathew@library.tamu.edu>
2020-11-02 17:26:23 -0600

    config json now available on dom content load

--- projects/wvr-elements/src/lib/shared/utility/bootstrap.utility.ts
+++ projects/wvr-elements/src/lib/shared/utility/bootstrap.utility.ts
@@ -2,10 +2,11 @@ import { Type } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppConfig, APP_CONFIG } from '../config';
 
+const componentScript = document.currentScript;
+
 /** Interigates the current script tag for its src and extroplates the configuration path from that location. */
 const obtainConfigPath = (): string => {
-  const componentScript = document.getElementsByTagName('script');
-  const componentScriptSrc = componentScript[componentScript.length - 1].src;
+  const componentScriptSrc = componentScript.getAttribute('src');
   const componentScriptSrcPathParts = componentScriptSrc.split('/');
   componentScriptSrcPathParts.pop();
   const configBasePath = componentScriptSrcPathParts.join('/');
@@ -14,7 +15,7 @@ const obtainConfigPath = (): string => {
 };
 
 /** Obtains, parses and injects the configuration. */
-const weaverBootstrap = (module: Type<unknown>) => () => fetch(obtainConfigPath())
+const weaverBootstrap = (module: Type<unknown>) => (configPath: string) => fetch(configPath ? configPath : obtainConfigPath())
   .then((response: Response) => response.json())
   .then((appConfig: AppConfig) => platformBrowserDynamic([{
     provide: APP_CONFIG,
