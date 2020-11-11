4820413b942d8e1b9da62e658c32228d147c6b1e
jeremythuff <jeremythuff@gmail.com>
2020-11-11 11:31:38 -0600

    WIP building bootstrap variables at build time

--- projects/wvr-elements/src/lib/shared/theme/default-theme.ts
+++ projects/wvr-elements/src/lib/shared/theme/default-theme.ts
@@ -1,29 +1,5 @@
 import { ThemeVariants } from '.';
+import * as defaultTheme from './default-theme.json';
 
 // tslint:disable-next-line:variable-name
-export const DefaultColorTheme: ThemeVariants = {
-  primary: {
-    baseColor: '#0d6efd'
-  },
-  secondary: {
-    baseColor: '#6c757d'
-  },
-  success: {
-    baseColor: '#198754'
-  },
-  danger: {
-    baseColor: '#dc3545'
-  },
-  warning: {
-    baseColor: '#ffc107'
-  },
-  info: {
-    baseColor: '#0dcaf0'
-  },
-  light: {
-    baseColor: '#f8f9fa'
-  },
-  dark: {
-    baseColor: '#212529'
-  }
-};
+export const DefaultColorTheme: ThemeVariants = defaultTheme;
