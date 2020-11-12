67dfae6b2e60b1de82c24295bbe37046d21d6edf
William Welling <wwelling@library.tamu.edu>
2020-11-12 08:36:52 -0600

    fix build-wvr-components

--- scripts/build-wvr-components.js
+++ scripts/build-wvr-components.js
@@ -4,28 +4,33 @@ const fs = require('fs-extra');
 const concat = require('concat');
 const cp = require('child_process');
 
-const assetPath = 'dist/weaver-components';
+const componentsPath = 'dist/weaver-components';
+const elementsPath = 'dist/wvr-elements';
+
 const bundlePath = 'dist/bundle';
+const usagePath = 'dist/static/docs/usage';
 
 cp.fork(__dirname + '/build-wvr-config-template.js');
 
 (async function build() {
   const files = [
-    'dist/weaver-components/runtime-es5.js',
-    'dist/weaver-components/polyfills-es5.js',
-    'dist/weaver-components/styles-es5.js',
-    'dist/weaver-components/scripts.js',
-    // 'dist/weaver-components/vendor-es5.js',
-    'dist/weaver-components/main-es5.js'
+    `${componentsPath}/runtime-es5.js`,
+    `${componentsPath}/polyfills-es5.js`,
+    `${componentsPath}/styles-es5.js`,
+    `${componentsPath}/scripts.js`,
+    // `${componentsPath}/vendor-es5.js`,
+    `${componentsPath}/main-es5.js`
   ];
 
   fs.ensureDir(bundlePath);
 
-  await concat(files, `${bundlePath}/weaver-components.js`);
-  fs.copy('dist/weaver-components/assets', "dist/static/docs/usage/assets");
-  fs.copy(`${assetPath}/assets`, `${bundlePath}/assets`);
+  fs.ensureDir(usagePath);
 
-  fs.copy('scripts', "dist/wvr-elements/scripts");
-  fs.copy('.wvr-ud', "dist/wvr-elements/.wvr-ud");
+  await concat(files, `${bundlePath}/weaver-components.js`);
+  fs.copy(`${componentsPath}/assets`, `${usagePath}/assets`);
+  fs.copy(`${componentsPath}/assets`, `${bundlePath}/assets`);
+
+  fs.copy('scripts', `${elementsPath}/scripts`);
+  fs.copy('.wvr-ud', `${elementsPath}/.wvr-ud`);
 
 })();
