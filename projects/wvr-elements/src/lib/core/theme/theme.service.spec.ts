63ef1b0b909f2c75805805e0e003aa7b6318b7b1
Rincy Mathew <rmathew@library.tamu.edu>
2020-11-19 08:58:58 -0600

    WIP - added a base theme service test file

---
+++ projects/wvr-elements/src/lib/core/theme/theme.service.spec.ts
@@ -0,0 +1,32 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed ,waitForAsync } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { metaReducers, ROOT_REDUCER } from '../store';
+import { ThemeService } from './theme.service';
+import { APP_CONFIG, testAppConfig } from '../../shared/config';
+import { WvrThemeableComponent } from '../../shared/wvr-themeable.component';
+
+describe('ThemeService', () => {
+  let service: ThemeService;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [BrowserAnimationsModule, StoreModule.forRoot(ROOT_REDUCER, { metaReducers })],
+      providers: [{
+        provide: APP_CONFIG,
+        useValue: testAppConfig
+      }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    })
+      .compileComponents()
+      .catch(err => { console.error(err); });
+    service = TestBed.inject(ThemeService);
+  }));
+
+  it('should be created', () => {
+    expect(service)
+      .toBeTruthy();
+  });
+
+});
