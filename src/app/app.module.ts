cabe7d76047feae72b2047ab619ecd68f775ba8d
William Welling <wwelling@library.tamu.edu>
2020-12-01 17:52:11 -0600

    weaver shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Injector, NgModule } from '@angular/core';
-import { registerCustomElements, showHiddentContent, WvrLibModule, wvrTimeout, WVR_ELEMENTS } from '../../projects/wvr-elements/src/public-api';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { registerCustomElements, showHiddentContent, WvrCoreModule, WvrSharedModule, wvrTimeout, WVR_ELEMENTS } from '../../projects/wvr-elements/src/public-api';
 
 @NgModule({
   imports: [
-    WvrLibModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // retains last 25 states
+      logOnly: true, // restrict extension to log-only mode
+    }),
+    WvrSharedModule,
+    WvrCoreModule
   ],
   exports: [],
   providers: [],
