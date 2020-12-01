ce1f98809c1ffcc0bb52c1172f2e315ad5b585d0
William Welling <wwelling@library.tamu.edu>
2020-12-01 17:59:02 -0600

    format polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,21 +2,21 @@
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
 
- /**
- * This file includes polyfills needed by Angular and is loaded before the app.
- * You can add your own extra polyfills to this file.
- *
- * This file is divided into 2 sections:
- *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
- *      file.
- *
- * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
- * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
- *
- * Learn more in https://angular.io/guide/browser-support
- */
+/**
+* This file includes polyfills needed by Angular and is loaded before the app.
+* You can add your own extra polyfills to this file.
+*
+* This file is divided into 2 sections:
+*   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
+*   2. Application imports. Files imported after ZoneJS that should be loaded before your main
+*      file.
+*
+* The current setup is for so-called "evergreen" browsers; the last versions of browsers that
+* automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
+* Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+*
+* Learn more in https://angular.io/guide/browser-support
+*/
 
 /***************************************************************************************************
  * BROWSER POLYFILLS
@@ -60,12 +60,17 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+
+(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+(window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
 
- // tslint:disable-next-line:no-import-side-effect
+// tslint:disable-next-line:no-import-side-effect
 import 'document-register-element';
 
