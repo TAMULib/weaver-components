844c2d5ec684b11065163f27775492440ad8d09c
Rincy Mathew <rmathew@library.tamu.edu>
2020-12-16 08:40:58 -0600

    WIP  - added unit test for manifest reducer

--- projects/wvr-elements/src/lib/core/manifest/manifest.reducers.spec.ts
+++ projects/wvr-elements/src/lib/core/manifest/manifest.reducers.spec.ts
@@ -4,28 +4,100 @@ import * as fromManifestReducers from './manifest.reducers';
 import * as fromManifestActions from './manifest.actions';
 
 describe('ManifestReducer', () => {
-  describe('undefined action', () => {
+  // initial state
+  describe('initial state', () => {
     it('should return the initial state', () => {
       const { initialState } = fromManifestReducers;
       const action = {} as any;
-      const state = fromManifestReducers.reducer(undefined, action);
-
-      expect(state)
+      expect(fromManifestReducers.reducer(undefined, action))
       .toBe(initialState);
     });
 
-    describe('selectManifestByName', () => {
-      it('should select manifest by name', () => {
-        const manifest1: Manifest = {
-          name: 'Manifest One',
-          baseUrl: 'http://www.google.com',
-          entries: []
-        };
+  });
 
-        expect(fromManifestReducers.selectManifestByName(manifest1))
-        .toEqual(manifest1.name);
-      });
+  describe('selectManifestByName', () => {
+    const manifestEntry1: ManifestEntry = {
+      name: "All Sorted",
+      path: "/ldap/all-sorted",
+      methods: ["GET"]
+    };
+    const manifestEntry2: ManifestEntry = {
+      name: "Departments",
+      path: "/ldap/departments",
+      methods: ["GET"]
+    };
+
+    const state =  {currentRequest: undefined,entities: {},ids: [],pendingRequests: []};
+    const manifest: Manifest = {
+      name: "Directory App",
+      baseUrl: "https://api-dev.library.tamu.edu/directory-service",
+      entries: [manifestEntry1, manifestEntry2]
+    };
+
+    // select manifest by name
+    it('should select manifest by name', () => {
+      expect(fromManifestReducers.selectManifestByName(manifest))
+      .toEqual(manifest.name);
+    });
+
+  });
+
+  describe('manifest reducers', () => {
+    const state =  {currentRequest: undefined,entities: {},ids: [],pendingRequests: []};
+    const action = { request: {manifestName: "Directory App", entryName: "All Sorted"}, type: "[Manifest] Submit Request" };
+    const manifestReducer  = {ids: [],entities: {},pendingRequests: [], currentRequest: {entryName: "All Sorted", manifestName: "Directory App"}};
+
+    it('manifest submit request', () => {
+      expect(fromManifestReducers.reducer(state, action))
+      .toEqual(manifestReducer);
+    });
+
+    it('manifest reducer gets current request', () => {
+      expect(fromManifestReducers.selectCurrentRequest(state))
+      .toEqual(state.currentRequest);
+    });
+
+    it('manifest reducer gets pending request', () => {
+      expect(fromManifestReducers.selectPendingRequests(state))
+      .toEqual(state.pendingRequests);
     });
   });
 
+  describe('manifest actions', () => {
+    const manifestEntry1: ManifestEntry = {
+      name: "All Sorted",
+      path: "/ldap/all-sorted",
+      methods: ["GET"]
+    };
+    const manifestEntry2: ManifestEntry = {
+      name: "Departments",
+      path: "/ldap/departments",
+      methods: ["GET"]
+    };
+    const manifest: Manifest = {
+      name: "Directory App",
+      baseUrl: "https://api-dev.library.tamu.edu/directory-service",
+      entries: [manifestEntry1, manifestEntry2]
+    };
+
+    const state = { ids: [], entities: {}, pendingRequests: [{manifestName: "Directory App", entryName: "All Sorted"}], currentRequest: undefined };
+    const action = { manifest: { manifest,entries: [
+            { description: undefined, map: data => data.payload[Object.keys(data.payload)[0]], methods: ["GET"], name: "All Sorted", options: {}, path: "/ldap/all-sorted"},
+            { description: undefined, map: data => data, methods: ["GET"], name: "Departments", options: {}, path: "/ldap/departments" }],
+        name: "Directory App"},
+     type: "[Manifest] Add Manifest"
+    };
+
+    it('add manifest action', () => {
+      const addManifestAction = fromManifestActions.addManifest({manifest});
+      Object.keys(addManifestAction).forEach(key => {
+        Object.keys(action).forEach(actionKey => {
+          if(key === actionKey && key === 'type') {
+            expect(addManifestAction[key])
+            .toEqual(action[key]);
+          }
+        });
+      });
+    });
+  });
 });
