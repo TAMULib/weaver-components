93c1b13160c1c734519731731b0d4a3e3077394c
Rincy Mathew <rmathew@library.tamu.edu>
2021-01-19 10:34:01 -0600

    WIP - added browser along with custom launcher option

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless', 'Chrome'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+          base: "ChromeHeadless",
+          flags: ["--no-sandbox"]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true,
     failOnEmptyTestSuite: false
