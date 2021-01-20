ef39279d142210d7830be636cfdf1e159d0dc2d6
Rincy Mathew <rmathew@library.tamu.edu>
2021-01-20 13:52:31 -0600

    WIP - removed istanbul reporter

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,18 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      // require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    preprocessors: {
+      "./!(node_modules)/**/*.js": "coverage"
+    },
+
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
+      type: "lcov",
       dir: require('path').join(__dirname, './static/weaver-components/reports/coverage/weaver-components'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
