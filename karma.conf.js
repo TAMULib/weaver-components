ade06ceec447f470f7845a5fdc8ff5c041f6e646
Rincy Mathew <rmathew@library.tamu.edu>
2021-01-21 09:32:04 -0600

    WIP - added chrome

--- karma.conf.js
+++ karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless', 'Chrome'],
     singleRun: false,
     restartOnFileChange: true,
     failOnEmptyTestSuite: false
