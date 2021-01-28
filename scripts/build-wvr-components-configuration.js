6497b1ef983220bb25990a7df332810ab3a058c6
Rincy Mathew <rmathew@library.tamu.edu>
2021-01-28 08:05:21 -0600

    WIP testing for config

--- scripts/build-wvr-components-configuration.js
+++ scripts/build-wvr-components-configuration.js
@@ -7,6 +7,8 @@ let defaults = dotEnv.config({
   path: `${process.cwd()}/defaults.env`
 });
 
+console.log('\n\n\n default config = ', defaults, '\n\n\n');
+
 if(process.argv[2]) {
   defaults = dotEnv.config({
     path: `${process.cwd()}/${process.argv[2]}`
@@ -23,8 +25,13 @@ Object.keys(defaults.parsed)
 if(fs.existsSync('./dist')) {
   fs.writeFile('./dist/bundle/config.json', configTemplate);
 }
-if(fs.existsSync('./static')) {
+
+if(fs.existsSync('./static')) { console.log(' \n\n\n config file copied to static path', configTemplate, '\n\n\n');
   fs.writeFile('./static/config.json', configTemplate);
+}
+
+if(fs.existsSync('./static/weaver-components')) { console.log(' \n\n\n config file copied to static/weaver-components path', configTemplate,  '\n\n\n');
+  fs.writeFile('./static/weaver-components/config.json', configTemplate);
 }
 
 fs.writeFile('./src/config.json', configTemplate);
