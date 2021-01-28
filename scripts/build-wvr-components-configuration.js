f085fb461741df579c4bbe01e1ffa682b767fad6
Rincy Mathew <rmathew@library.tamu.edu>
2021-01-28 09:04:06 -0600

    WIP testing for static wc and docs usage config

--- scripts/build-wvr-components-configuration.js
+++ scripts/build-wvr-components-configuration.js
@@ -24,9 +24,17 @@ if(fs.existsSync('./dist')) {
   fs.writeFile('./dist/bundle/config.json', configTemplate);
 }
 
-if(fs.existsSync('./static')) { console.log(' \n\n\n config file copied to static path', configTemplate, '\n\n\n');
+if(fs.existsSync('./static')) {
+  console.log(' \n\n\n config file copied to static path', configTemplate, '\n\n\n');
   fs.writeFile('./static/config.json', configTemplate);
+  console.log(' \n\n\n config file copied to static path', configTemplate, '\n\n\n');
   fs.writeFile('./static/weaver-components/config.json', configTemplate);
+  console.log(' \n\n\n config file copied to static weaver docs path \n\n\n');
+  fs.writeFile('./static/weaver-components/docs/config.json', configTemplate);
+  console.log(' \n\n\n config file copied to static weaver docs usage path \n\n\n');
+  fs.writeFile('./static/weaver-components/docs/usage/config.json', configTemplate);
+  console.log(' \n\n\n config file copied to static weaver docs assets path \n\n\n');
+  fs.writeFile('./static/weaver-components/docs/assets/config.json', configTemplate);
 }
 
 if(fs.existsSync('./weaver-components')) {
