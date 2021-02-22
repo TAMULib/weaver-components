0de73dca703ced3bde1891cd05e11a2b1a41e88f
Rincy Mathew <rmathew@library.tamu.edu>
2021-02-22 16:23:55 -0600

    WIP - added a test

--- projects/wvr-elements/src/lib/wvr-modal/wvr-modal.component.spec.ts
+++ projects/wvr-elements/src/lib/wvr-modal/wvr-modal.component.spec.ts
@@ -1,25 +1,69 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideMockStore } from '@ngrx/store/testing';
+import { APP_CONFIG, testAppConfig } from '../shared/config';
 import { WvrModalComponent } from './wvr-modal.component';
 
+
+@Component({
+  selector: 'wvr-modal-host-component',
+  // tslint:disable-next-line:component-max-inline-declarations
+  template: `<wvr-modal-component>
+            </wvr-modal-component>`
+})
+class WvrModalHostComponent {
+  @ViewChild(WvrModalComponent) modal: WvrModalComponent;
+}
 describe('WvrModalComponent', () => {
+  const initialState = { theme: {
+    themes: {}
+  }};
+
   let component: WvrModalComponent;
   let fixture: ComponentFixture<WvrModalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ WvrModalComponent ]
+  let hostComponent: WvrModalHostComponent;
+  let hostFixture: ComponentFixture<WvrModalHostComponent>;
+
+  beforeEach(waitForAsync(() => {
+
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: testAppConfig
+        },
+        provideMockStore({initialState})
+      ],
+      declarations: [
+        WvrModalHostComponent,
+        WvrModalComponent
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
-  });
+      .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WvrModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    hostFixture = TestBed.createComponent(WvrModalHostComponent);
+    hostComponent = hostFixture.componentInstance;
+    hostFixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have themeVariant defined', () => {
+    expect(component.themeVariant)
+      .toEqual('primary');
   });
 });
