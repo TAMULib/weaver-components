998cdd1d7fad2dc24aaf20d12f490f30c1f0fa8e
Rincy Mathew <rmathew@library.tamu.edu>
2021-02-24 10:40:41 -0600

    WIP - added modal size customize option, themeVariant customizing available

--- projects/wvr-elements/src/lib/wvr-modal/wvr-modal.component.ts
+++ projects/wvr-elements/src/lib/wvr-modal/wvr-modal.component.ts
@@ -20,8 +20,20 @@ export class WvrModalComponent extends WvrBaseComponent implements AfterViewInit
   /** Indicates the presence of the modal footer in the projected content. */
   hasModalFooter: boolean;
 
-  /** Used to define the class type for button component.  */
+  /** Used to define the theme type for modal component. */
   @Input() themeVariant: ThemeVariantName = 'primary';
+
+  /** Used to define the class type for modal header section. */
+  @Input() modalHeaderThemeVariant: ThemeVariantName = 'success';
+
+  /** Used to define the class type for modal body section. */
+  @Input() modalBodyThemeVariant: ThemeVariantName = 'info';
+
+  /** Used to define the class type for modal footer section. */
+  @Input() modalFooterThemeVariant: ThemeVariantName = 'warning';
+
+  /** Used to define the size for modal component. */
+  @Input() modalSize: 'lg' | 'sm' = 'lg';
 
   constructor(injector: Injector) {
     super(injector);
@@ -35,7 +47,7 @@ export class WvrModalComponent extends WvrBaseComponent implements AfterViewInit
     });
   }
 
-  /** Prepares the card header for display, and sets it to the DOM */
+  /** Used to render the modal header section if present */
   private renderModalHeader(): void {
     const wvrModalHeaderElem = this.eRef.nativeElement.querySelector('wvre-modal-header');
     if (wvrModalHeaderElem) {
@@ -45,11 +57,50 @@ export class WvrModalComponent extends WvrBaseComponent implements AfterViewInit
     }
   }
 
+  /** Used to render the modal footer section if present */
   private renderModalFooter(): void {
     const wvrModalFooterElem = this.eRef.nativeElement.querySelector('wvre-modal-footer');
     if(wvrModalFooterElem) {
       this.hasModalFooter=true;
     }
+  }
+
+  /** Used to customize the css properties for modal header based on theme variant. */
+  additionalModalHeaderClasses(): string {
+    let additionalClasses = '';
+    additionalClasses += this.modalHeaderThemeVariant ?
+                        ` border-${this.modalHeaderThemeVariant} bg-${this.modalHeaderThemeVariant} `
+                        : ` border-${this.themeVariant} bg-${this.themeVariant} `;
+    additionalClasses += this.getTextColorByThemeVariant(this.modalHeaderThemeVariant, this.themeVariant);
+    return additionalClasses;
+  }
+
+  /** Used to customize the css properties for modal body based on theme variant. */
+  additionalModalBodyClasses(): string {
+    let additionalClasses = '';
+    additionalClasses += this.modalBodyThemeVariant ?
+                        ` border-${this.modalBodyThemeVariant} bg-${this.modalBodyThemeVariant} `
+                        : ` border-${this.themeVariant} bg-${this.themeVariant} `;
+    additionalClasses += this.getTextColorByThemeVariant(this.modalBodyThemeVariant, this.themeVariant);
+    return additionalClasses;
+  }
+
+  /** Used to customize the css properties for modal footer based on theme variant. */
+  additionalModalFooterClasses(): string {
+    let additionalClasses = '';
+    additionalClasses += this.modalFooterThemeVariant ?
+                        ` border-${this.modalFooterThemeVariant} bg-${this.modalFooterThemeVariant} `
+                        : ` border-${this.themeVariant} bg-${this.themeVariant} `;
+    additionalClasses += this.getTextColorByThemeVariant(this.modalFooterThemeVariant, this.themeVariant);
+    return additionalClasses;
+  }
+
+  /** Used to customize the text color property for the modal based on theme variant. */
+  getTextColorByThemeVariant(providedTheme, defaultTheme): string {
+    let textClass = '';
+    let themeClass = ( !(typeof providedTheme === undefined) && (providedTheme)) ? providedTheme : defaultTheme ;
+    textClass += ( (themeClass == 'warning') || (themeClass == 'light')) ? 'text-dark' : 'text-white';
+    return textClass;
   }
 
 }
