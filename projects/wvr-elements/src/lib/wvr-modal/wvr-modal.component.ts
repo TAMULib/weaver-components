c523170fdc6789e063db3c476c7544b7fad4ae85
jeremythuff <jeremythuff@gmail.com>
2021-03-10 10:46:42 -0600

    Added subscription to modal

--- projects/wvr-elements/src/lib/wvr-modal/wvr-modal.component.ts
+++ projects/wvr-elements/src/lib/wvr-modal/wvr-modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, Injector, Input, OnInit } from '@angular/core';
 import { WvrBaseComponent } from '../shared/wvr-base.component';
 import * as ModalActions from '../core/modal/modal.actions';
+import { select, Store } from '@ngrx/store';
+import { filter } from 'rxjs/operators';
+import { RootState, selectModalState } from '../core/store';
 
 @Component({
   selector: 'wvr-modal',
@@ -21,6 +24,27 @@ export class WvrModalComponent extends WvrBaseComponent implements OnInit {
       name: this.name,
       open: false
     }}));
+    this.store.pipe(
+      select(selectModalState),
+      filter(modalState => !!modalState),
+      filter(modalState => modalState.modal.name === this.name)
+    )
+    .subscribe(modalState => {
+      if (modalState.modal.open) {
+        //open modal
+      } else {
+        //clode modal
+      }
+    });
+  }
+
+  openModal() : void {
+    this.store.dispatch(ModalActions.openModal({
+      modal: {
+        name: this.name,
+        open: true
+      }
+    }));
   }
 
 }
