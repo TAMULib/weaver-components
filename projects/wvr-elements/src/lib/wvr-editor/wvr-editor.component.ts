cf7a17176b9c8391506c9b888c7882f54a754cde
Rincy Mathew <rmathew@library.tamu.edu>
2021-03-16 15:04:34 -0500

    WIP - modified the editor component

--- projects/wvr-elements/src/lib/wvr-editor/wvr-editor.component.ts
+++ projects/wvr-elements/src/lib/wvr-editor/wvr-editor.component.ts
@@ -1,13 +1,18 @@
-import { AfterViewInit, Component, EventEmitter, Injector, Input, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, Injector, Input, Output } from '@angular/core';
 import tinymce from 'tinymce';
 import { WvrBaseComponent } from '../shared/wvr-base.component';
+import { WvrEditorMenu } from './wvr-editor-menu';
+import { WvrEditorSubMenu } from './wvr-editor-sub-menu';
+import * as WvrEditor from './wvr-editor.json';
+import * as JSON5 from 'json5';
 
 @Component({
   selector: 'wvr-editor-component',
   templateUrl: './wvr-editor.component.html',
-  styleUrls: ['./wvr-editor.component.scss']
+  styleUrls: ['./wvr-editor.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WvrEditorComponent extends WvrBaseComponent implements AfterViewInit {
+export class WvrEditorComponent extends WvrBaseComponent {
 
   htmlId = `wvr-editor-${this.id}`;
 
@@ -35,16 +40,12 @@ export class WvrEditorComponent extends WvrBaseComponent implements AfterViewIni
 
   @Input() toolBar = ['undo redo | formatselect | bold italic underline code | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | help | print preview | media spellchecker' ];
 
-  @Input() menu: Object = `{
-                            file: { title: 'File', items: 'newdocument restoredraft | preview | print ' },
-                            edit: { title: 'Edit', items: 'undo redo | cut copy paste | selectall | searchreplace' },
-                            view: { title: 'View', items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen' },
-                            insert: { title: 'Insert', items: 'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
-                            format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontformats fontsizes align lineheight | forecolor backcolor | removeformat' },
-                            tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | code wordcount' },
-                            table: { title: 'Table', items: 'inserttable | cell row column | tableprops deletetable' },
-                            help: { title: 'Help', items: 'help'
-                          }`;
+  private _menu: WvrEditorMenu;
+
+  @Input() set menu(editorMenu: string) {
+    this._menu = JSON5.parse(editorMenu) as WvrEditorMenu;
+    console.log(this._menu);
+  }
 
   @Input() mobileMenu: Object = `{
                                   menubar: true,
@@ -57,36 +58,6 @@ export class WvrEditorComponent extends WvrBaseComponent implements AfterViewIni
 
   constructor(injector: Injector) {
     super(injector);
-  }
-
-  ngAfterViewInit(): void {
-    tinymce.init({
-      selector: `textarea#${this.htmlId}`,
-      initialValue: this.initialValue,
-      plugins: this.editorPlugings,
-      skin_url: this.skinUrl,
-      menubar: this.menuBar,
-      toolbar: this.toolBar
-      /* enable title field in the Image dialog*/
-      // image_title: true,
-      /* enable automatic uploads of images represented by blob or data URIs*/
-      // automatic_uploads: true,
-
-    });
-
-    tinymce.init({
-      selector: `${this.htmlId}`,
-      initialValue: this.initialValue,
-      plugins: this.editorPlugings,
-      skin_url: this.skinUrl,
-      menubar: this.menuBar,
-      toolbar: this.toolBar
-      /* enable title field in the Image dialog*/
-      // image_title: true,
-      /* enable automatic uploads of images represented by blob or data URIs*/
-      // automatic_uploads: true,
-
-    });
   }
 
 }
