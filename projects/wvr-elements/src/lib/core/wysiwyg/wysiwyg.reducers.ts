82f3be7e0f665810d4f1aa4001299d826e734630
jeremythuff <jeremythuff@gmail.com>
2021-03-25 16:49:29 -0500

    Added resetWysiwyg reducer

--- projects/wvr-elements/src/lib/core/wysiwyg/wysiwyg.reducers.ts
+++ projects/wvr-elements/src/lib/core/wysiwyg/wysiwyg.reducers.ts
@@ -1,15 +1,10 @@
-import { state } from "@angular/animations";
-import { createEntityAdapter, EntityAdapter, EntityState } from "@ngrx/entity";
-import { createReducer, on } from "@ngrx/store";
-import { Wysiwyg } from "./wysiwyg";
+
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import { createReducer, on } from '@ngrx/store';
+import { Wysiwyg } from './wysiwyg';
 import * as WysiwygActions from './wysiwyg.actions';
 
 export interface State extends EntityState<Wysiwyg> {}
-
-// export interface State {
-//   wysiwyg: { [name: string]: Wysiwyg };
-//   currentWysiwyg: string;
-// }
 
 export function selectWysiwygById(wysiwyg: Wysiwyg): string {
   return wysiwyg.id;
@@ -24,39 +19,18 @@ export const initialState: State = adapter.getInitialState();
 export const reducer = createReducer(
   initialState,
   on(WysiwygActions.addWysiwyg, (state, { wysiwyg }) => adapter.addOne(wysiwyg, state)),
-  on(WysiwygActions.saveWysiwyg, ( state, { id, content }) => adapter.updateOne({
+  on(WysiwygActions.saveWysiwyg, (state, { id, content }) => adapter.updateOne({
     id,
     changes: {
       content
     }
   },
   {...state})),
-  on(WysiwygActions.resetWysiwyg, (state, { id}) =>  adapter.updateOne({
+  on(WysiwygActions.resetWysiwyg, (state, {id}) =>  adapter.updateOne({
     id,
     changes: {
-      // content: state.entities.get(id);
+      content: state.entities[id].initialContent
     }
   },
   {...state}))
 );
-
-// export const reducer = createReducer(
-//   initialState,
-//   on(WysiwygActions.addModal, (state, { modal }) => adapter.addOne(modal, state)),
-//   on(ModalActions.closeModal, (state, { id }) => adapter.updateOne({
-//     id,
-//     changes: {
-//       open: false
-//     }
-//   }, {
-//     ...state
-//   })),
-//   on(ModalActions.openModal, (state, { id }) => adapter.updateOne({
-//     id,
-//     changes: {
-//       open: true
-//     }
-//   }, {
-//     ...state
-//   }))
-// );
--- projects/wvr-elements/src/lib/core/wysiwyg/wysiwyg.ts
+++ projects/wvr-elements/src/lib/core/wysiwyg/wysiwyg.ts
@@ -3,5 +3,3 @@ export interface Wysiwyg {
   initialContent: string;
   content: string;
 }
-
-
