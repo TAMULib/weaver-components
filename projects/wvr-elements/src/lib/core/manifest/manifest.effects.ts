0d51a1d6c9ddbcb9f7f60f153b13779c88b9c474
William Welling <wwelling@library.tamu.edu>
2021-05-13 21:20:29 -0500

    add missing mapping from manifest request

--- projects/wvr-elements/src/lib/core/manifest/manifest.effects.ts
+++ projects/wvr-elements/src/lib/core/manifest/manifest.effects.ts
@@ -53,6 +53,7 @@ export class ManifestEffects {
             url,
             method,
             options,
+            bodyHttpParams: request.bodyHttpParams,
             map: entry.map
           },
           success: response => onSuccess.concat(ManifestActions.submitRequestSuccess({ manifest, request, response })),
