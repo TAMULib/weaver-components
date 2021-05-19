6e90d9d06bd16072a3dca6fc516ac7d4d676ada9
William Welling <wwelling@library.tamu.edu>
2021-05-19 10:55:42 -0500

    minor formatting of script

--- scripts/build-publish.js
+++ scripts/build-publish.js
@@ -5,24 +5,18 @@ const shell = require('shelljs')
 const angularCli = require('@angular/cli');
 const elementsPath = 'dist/wvr-elements';
 
-const next = process.argv[2] ?
-             '--tag next' :
-             '';
+const next = process.argv[2] ? '--tag next' : '';
 
 angularCli.default({
-    cliArgs: [
-      'b',
-      '--project=wvr-elements'
-    ],
-    inputStream: process.stdin,
-    outputStream: process.stdout
-  }).then(c => {
-    fs.copySync('projects/wvr-elements/src/lib/shared/styles', `${elementsPath}/styles`);
-    fs.copySync('scripts', `${elementsPath}/scripts`);
-    fs.copySync('.wvr-ud', `${elementsPath}/.wvr-ud`);
+  cliArgs: ['b', '--project=wvr-elements'],
+  inputStream: process.stdin,
+  outputStream: process.stdout
+}).then(c => {
+  fs.copySync('projects/wvr-elements/src/lib/shared/styles', `${elementsPath}/styles`);
+  fs.copySync('scripts', `${elementsPath}/scripts`);
+  fs.copySync('.wvr-ud', `${elementsPath}/.wvr-ud`);
 
-    shell.exec(`npm publish ${elementsPath}/ ${next}`);
+  shell.exec(`npm publish ${elementsPath}/ ${next}`);
 
-    shell.exit();
-
-  });
+  shell.exit();
+});
