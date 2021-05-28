b57eda06c75ae905acf8e45eead6916d2f8763be
Jeremy Huff <jeremythuff@gmail.com>
2021-05-28 16:03:26 -0500

    Added path params to entry options

--- projects/wvr-elements/src/lib/core/rest/request-options.ts
+++ projects/wvr-elements/src/lib/core/rest/request-options.ts
@@ -4,6 +4,7 @@ export interface RequestOptions {
   headers?: { [header: string]: string | Array<string>; };
   observe?: 'body' | 'response' | 'events';
   params?: HttpParams | { [param: string]: string | Array<string>; };
+  pathVariables?: Map<string, string>;
   reportProgress?: boolean;
   responseType?: 'arraybuffer' | 'blob' | 'text' | 'json';
   withCredentials?: boolean;
